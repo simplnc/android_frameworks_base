b19e8936148a7932610050951cda87b862788aa3
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-09 20:53:52 +0200

    [1/2] Sensor : Sensor block per-package switch
    aicp edits:
    add facebook messenger, and slack
    
    Change-Id: I88ece292a6ef6867577b2e9cca5ea5e2d1d471b2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    FWB: Sensor block per-package switch (1/2)
    
    Change-Id: I3a5cb8820ca6815c7140c25368e525faa2ecc5e6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    base: Add Accelerometer and Linear Acceleration sensors in blocking list
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6675,6 +6675,13 @@ public final class Settings {
         public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        @Readable
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
