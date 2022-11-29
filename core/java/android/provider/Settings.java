a11bdb89944cec4de6e37e0a8773f38769eaeff9
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-11-29 22:03:03 +0530

    [1/2] vibr : Add optional haptic feedback to brightness slider
    Co-Authored-By: Barba_tech <91166081+ageuqxb@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6693,6 +6693,12 @@ public final class Settings {
         public static final String SENSOR_BLOCKED_APP_DUMMY = "sensor_blocked_app_dummy";
 
         /**
+         * Haptic feedback on brightness slider
+         * @hide
+         */ 
+        public static final String BRIGHTNESS_SLIDER_HAPTIC_FEEDBACK = "brightness_slider_haptic_feedback";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
