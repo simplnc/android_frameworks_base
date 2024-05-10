49a52953865e76c1e232b1dfcf35180ae0eed0c7
Anay Wadhera <anay1018@gmail.com>
2024-05-10 18:38:12 +0000

    SystemUI/QSTILES : Introduce Flashlight Strength Tile
    Squashed:
    * FlashlightStrengthTile: Ignore sliding when tile is inactive
    * FlashlightStrengthTile: Use correct default value for percentage drawable
    * FlashlightStrengthTile: Hide percentage when tile is inactive
    * FlashlightStrengthTile: Don't turn off for percentage > 0
    * FlashlightStrengthTile: Refresh state on torch strength level changes
    * SliderQSTile: Use different bg color for =>90%
    * QSTileViewImpl: Allow to override getBackgroundColorForState()
    * QsTileViewImpl: Make setColor() as protected
    * Update to Android 15
    
    Change-Id: Ic9b423f1701b0047c148e90a772db84f8dea0b35
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    SystemUI: Improve Slider tiles percentage color
    
    SystemUI: Fix flashlight strength tile in secondary user
    
    Use {get,put}FloatForUser to ensure we set/get the user-specific
    strength setting. Fixes slider getting stuck in secondary user.
    
    Change-Id: I937da7d82d396f560ad3ff90fcd66bba2dc883dc
    
    FlashlightControllerImpl: Fix crash when camera IDs list is null
    
    07-15 10:09:59.711 E/CameraManagerGlobal(4283): Camera ID array is null
    07-15 10:09:59.711 E/FlashlightController(4283): Couldn't initialize.
    07-15 10:09:59.711 E/FlashlightController(4283): java.lang.NullPointerException: Attempt to get length of null array
    07-15 10:09:59.711 E/FlashlightController(4283): 	at com.android.systemui.statusbar.policy.FlashlightControllerImpl.getCameraId(FlashlightControllerImpl.java:179)
    07-15 10:09:59.711 E/FlashlightController(4283): 	at com.android.systemui.statusbar.policy.FlashlightControllerImpl.tryInitCamera(FlashlightControllerImpl.java:113)
    07-15 10:09:59.711 E/FlashlightController(4283): 	at com.android.systemui.statusbar.policy.FlashlightControllerImpl.$r8$lambda$Tg52KqU0V7WN8YyLdqR6Wubie2M(FlashlightControllerImpl.java:0)
    07-15 10:09:59.711 E/FlashlightController(4283): 	at com.android.systemui.statusbar.policy.FlashlightControllerImpl$$ExternalSyntheticLambda0.run(R8$$SyntheticClass:0)
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: donjohanliebert <donjohanliebert@gmail.com>
    Co-Authored-By: Jake Weinstein <jake@aospa.co>
    Co-Authored-By: ralph950412 <ralph950412@gmail.com>
    Co-Authored-By: Dhina17 <dhinalogu@gmail.com>
    
    FlashlightStrengthTile: TouchableQSTile -> SlideableQSTile
    
    Change-Id: I1d0e9c4db20f37b09828fd582dd6c289fefda6b6
    
    SliderQSTileView: Pass the SlideableQSTile interface itself
    
    Before we pass the required values to the constructor
    at the time of creation.
    The values may change due to the race condition like flashlight
    controller not available at the time of construction.
    But we need proper values from TouchableQSTile interface to initialize
    SliderQSTileViewImpl and wherever it's used.
    
    Change-Id: Ic1c70fe2eae7666b22779d49c83010df57425af1
    
    FlashlightStrengthTile: Update bg color only if supports flash strength control
    
    Avoid color changes in flashlight tile when device doesn't support
    flashlight strength control.
    
    Change-Id: Ibb8a4ac1c98360e6a674c87144248ee6afde6bda
    
    fixup: SystemUI: Improve Slider tiles percentage color
    
    Change-Id: Ie657be72048a6bcd021f2be18b57a8ac96929c4b
    
    SliderQSTileViewImpl: Update tint when updating resources
    
    Change-Id: I40e8c7c52301d23145ef836dbc288b3aa19f30a1

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -40,7 +40,10 @@ import com.android.systemui.plugins.qs.QSTileView;
 import com.android.systemui.qs.customize.QSCustomizerController;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.qs.tiles.FlashlightStrengthTile;
 import com.android.systemui.qs.tileimpl.QSTileViewImpl;
+import com.android.systemui.qs.tileimpl.SliderQSTileViewImpl;
+import com.android.systemui.qs.tileimpl.SlideableQSTile;
 import com.android.systemui.scene.shared.flag.SceneContainerFlag;
 import com.android.systemui.statusbar.policy.SplitShadeStateController;
 import com.android.systemui.util.ViewController;
@@ -370,8 +373,18 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
         } else {
             longPressEffect = null;
         }
-        final QSTileViewImpl tileView = new QSTileViewImpl(
-                getContext(), collapsedView, longPressEffect);
+        final QSTileViewImpl tileView;
+        if (FlashlightStrengthTile.TILE_SPEC.equals(tile.getTileSpec())
+            || VolumeControlTile.TILE_SPEC.equals(tile.getTileSpec())) {
+            SlideableQSTile slideableQSTile = (SlideableQSTile) tile;
+            tileView = new SliderQSTileViewImpl(
+                    getContext(),
+                    collapsedView,
+                    slideableQSTile);
+        } else {
+            tileView = new QSTileViewImpl(
+                    getContext(), collapsedView, longPressEffect);
+        }
         final TileRecord r = new TileRecord(tile, tileView);
         // TODO(b/250618218): Remove the QSLogger in QSTileViewImpl once we know the root cause of
         // b/250618218.
