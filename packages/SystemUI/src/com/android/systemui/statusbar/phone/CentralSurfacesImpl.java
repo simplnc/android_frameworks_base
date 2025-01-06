883ec6f6f9b8cd390f0fb107004244fc8591209c
Anna-Sophie Kasierocka <hanna.zaitsewa@gmail.com>
2025-01-06 21:40:25 +0100

    SystemUI/StatusBar : Add ongoing action chip
    Ongoing action progressbar chip implementation
    
    This commit provides an implementation of a progressbar chip in statusbar which features ongoing action progress from a notification
    The chip is featured to the right of default statusbar clock position, but before the icon container. The contents of chip are miniature app icon and a progress bar
    The implementation provided is basic and lacks customizations which are planned to be added further
    Idea is taken from this reddit post: https://www.reddit.com/r/NOTHING/comments/1dyuak6/nothing_os_30_images_fan_made/
    
    Change-Id: Ide3ff14927b314d9b988e2c24eba5345fad168aa
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: osm1019 <osm1019@gmail.com>
    Signed-off-by: elpaablo <donopwhile1@gmail.com>
    
    OnGoingActionProgressController: Fix potential null key comparison
    
    Change-Id: I2bba4d8d1f6f94402edd2235f3677ef8375a39fb
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    SystemUI: Add ongoing action chip toggle setting [1/2]
    
    Change-Id: I2083b5184d9a8233722661e42d7d5f33876fc276
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: elpaablo <donopwhile1@gmail.com>
    
    SystemUI: Don't tint progressbar chip icon
    
    Change-Id: I5de1ff070e8ced513db5e4867728fcd727e5f511
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    SystemUI: Hide ongoing action progress chip on lockscreen
    
    Change-Id: Iace5817e4c3111d23a1c38c0707e4822ce8fd93c
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: elpaablo <donopwhile1@gmail.com>
    
    SystemUI: Reset ongoing action progress state on notification removal
    
    Change-Id: I9e7bf81c25e5efa8578aefc6f485fc7eb92fc164
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    OnGoingActionProgressController: Store current drawable for icon
    
    Change-Id: I8308dd789d06781c1d1a174dfe304c1facafa35d
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    
    OnGoingActionProgressController: Remove redundant drawable setting in updateViews()
    
    As it's already set in updateIconImageView.
    
    Change-Id: Ia467e4ff8bb0ec8ec5251a080a7073e3d4798c2f
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Rve27 <rve27github@gmail.com>
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -189,12 +189,14 @@ import com.android.systemui.statusbar.KeyguardIndicationController;
 import com.android.systemui.statusbar.LiftReveal;
 import com.android.systemui.statusbar.LightRevealScrim;
 import com.android.systemui.statusbar.LockscreenShadeTransitionController;
+import com.android.systemui.statusbar.NotificationListener;
 import com.android.systemui.statusbar.NotificationLockscreenUserManager;
 import com.android.systemui.statusbar.NotificationMediaManager;
 import com.android.systemui.statusbar.NotificationPresenter;
 import com.android.systemui.statusbar.NotificationRemoteInputManager;
 import com.android.systemui.statusbar.NotificationShadeDepthController;
 import com.android.systemui.statusbar.NotificationShadeWindowController;
+import com.android.systemui.statusbar.OnGoingActionProgressController;
 import com.android.systemui.statusbar.PowerButtonReveal;
 import com.android.systemui.statusbar.PulseExpansionHandler;
 import com.android.systemui.statusbar.StatusBarState;
@@ -291,6 +293,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                     boolean occluded = mKeyguardStateController.isOccluded();
                     mStatusBarHideIconsForBouncerManager.setIsOccludedAndTriggerUpdate(occluded);
                     mScrimController.setKeyguardOccluded(occluded);
+                    Log.d(TAG, "occluded=" + occluded);
                 }
             };
 
@@ -377,6 +380,10 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
     DozeServiceHost mDozeServiceHost;
     private final LightRevealScrim mLightRevealScrim;
     private PowerButtonReveal mPowerButtonReveal;
+
+    private OnGoingActionProgressController mOnGoingActionProgressController = null;
+
+    @Inject public NotificationListener mNotificationListener;
 
     /**
      * Whether we should delay the wakeup animation (which shows the notifications and moves the
@@ -1224,6 +1231,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
                         mShadeSurface.updateExpansionAndVisibility();
                         setBouncerShowingForStatusBarComponents(mBouncerShowing);
                         checkBarModes();
+                        mOnGoingActionProgressController =
+                                new OnGoingActionProgressController(
+                                        mContext,
+                                        statusBarViewController.getOngoingActionProgressGroup(),
+                                        mNotificationListener, mKeyguardStateController);
                     });
         }
         if (!StatusBarRootModernization.isEnabled() && !StatusBarConnectedDisplays.isEnabled()) {
