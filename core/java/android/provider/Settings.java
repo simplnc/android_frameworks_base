07a35ff4684c3b8f0e8ec24dcb94dda8e3d8e01c
Ghosuto <clash.raja10@gmail.com>
2025-03-21 16:25:28 +0000

    SYSTEM-CORE : StatusBar : Introduce media playback progress bar in action chip
    SystemUI: Media Playback Enhancements
    
    Added media playback progress display in the status bar.
    
    Integrated MediaSessionManagerHelper (thanks to RisingOS and AxionAOSP) to track playback state.
    
    Media progress shown only during active playback, hidden during downloads.
    
    Gesture Controls
    
    Single tap: Stop media.
    
    Double tap: Skip to next track / Play-Pause.
    
    Long press: Open the media app.
    
    Swipe gestures to change tracks.
    
    UI Improvements
    
    Added ripple effect to overlay action container background.
    
    Removed unused foreground drawable of ongoing chip.
    
    Optimized code and improved OngoingChip handling.
    
    Minor improvements in ongoing chip UI and ripple effect.
    
    Increased progress chip size.
    
    Updated cr_dimens.xml.
    
    Added missing drawable resources and fixed minor UI issues.
    
    Author: Ghosuto clash.raja10@gmail.com
    
    Progress indicator: dont track notifications when feature off
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Co-Authored-By: Barba_tech <91166081+ageuqxb@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6679,6 +6679,11 @@ public final class Settings {
         public static final String ONGOING_ACTION_CHIP = "ongoing_action_chip";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_MEDIA_PROGRESS = "show_media_progress";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
