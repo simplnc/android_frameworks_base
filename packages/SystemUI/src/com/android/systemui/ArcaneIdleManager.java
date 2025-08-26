afb8d5fc9f0732024a69177f2afe213ca7d738d3
The-projects <simplncprivate@gmail.com>
2025-08-26 22:12:45 +0100

    [1/2] BackgroundManager : Implement background process killed
    This reverts commit fe4b21afbcd707e3dba441e1dda4dd00681ff6b9.
    
    [1/2] OrchidOs Implement background process killer [1/2]
    
    * rebranded/adapted from Descendant-XI/frameworks_base@4d6a3fa * nuked stuffs we dont want to mess with like sync and location
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    
    IdleManager: CleanUp
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    
    IdleManager: update packagelist
    
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>
    
    Update ArcaneIdleManager.java
    
    we will add more apps as requested by testers
    such as snapchat, telegram and instagram and more

--- packages/SystemUI/src/com/android/systemui/ArcaneIdleManager.java
+++ packages/SystemUI/src/com/android/systemui/ArcaneIdleManager.java
@@ -1,46 +1,142 @@
-public static void servicesKiller() {
-    localActivityManager = (ActivityManager) imContext.getSystemService(Context.ACTIVITY_SERVICE);
-    RunningServices = localActivityManager.getRunningAppProcesses();
-    if (RunningServices == null) return;
+/*
+ * Copyright (C) 2019 Descendant
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-    // Whitelist prefixes
-    Set<String> whitelistPrefixes = new HashSet<>(Arrays.asList(
-        "com.android.", "com.google.", "com.mgoogle.", "com.vanced.",
-        "zhihu.", "ugc.", "GoogleCamera.", "com.whatsapp.",
-        "com.spotify.", "com.amazon.", "com.twitter.", "com.netflix.",
-        "com.discord.", "com.reddit.", "com.monzo.", "com.barclays.",
-        "com.natwest.", "com.paypal."
-    ));
+package com.android.systemui;
 
-    // Whitelist exact matches
-    Set<String> whitelistExact = new HashSet<>(Arrays.asList(
-        "org.telegram", "org.thoughtcrime.securesms",
-        "chat.delta", "com.wire", "ims"
-    ));
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.AlarmManager.AlarmClockInfo;
+import android.app.UiModeManager;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.util.Log;
 
-    for (ActivityManager.RunningAppProcessInfo procInfo : RunningServices) {
-        if (procInfo.pkgList == null) continue;
+import com.android.systemui.Dependency;
 
-        for (String pkg : procInfo.pkgList) {
-            boolean skip = false;
+import java.util.ArrayList;
+import java.util.List;
 
-            // Check prefixes
-            for (String prefix : whitelistPrefixes) {
-                if (pkg.startsWith(prefix)) {
-                    skip = true;
-                    break;
-                }
+public class ArcaneIdleManager {
+    static String TAG = "ArcaneIdleManager";
+
+    static Handler h = new Handler();
+    static Runnable rStateTwo;
+    static Runnable rStateThree;
+    static List<ActivityManager.RunningAppProcessInfo> RunningServices;
+    static ActivityManager localActivityManager;
+    static Context imContext;
+    static ContentResolver mContentResolver;
+    static List<String> killablePackages;
+    static final long IDLE_TIME_NEEDED = 4000000;
+    static int ultraSaverStatus;
+
+    public static void initManager(Context mContext) {
+        imContext = mContext;
+        killablePackages = new ArrayList<>();
+        localActivityManager = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        mContentResolver = mContext.getContentResolver();
+
+        rStateTwo = new Runnable() {
+            public void run() {
+                    servicesKiller();
             }
-
-            // Check exact
-            if (!skip && whitelistExact.contains(pkg)) {
-                skip = true;
+        };
+        rStateThree = new Runnable() {
+            public void run() {
+                haltManager();
             }
+        };
+    }
 
-            if (!skip) {
-                localActivityManager.killBackgroundProcesses(pkg);
-                Log.d(TAG, "Killed background process: " + pkg);
+    public static void executeManager() {
+        String TAG_SUBCLASS = "executeManager ";
+        RunningServices = localActivityManager.getRunningAppProcesses();
+
+        if (IDLE_TIME_NEEDED > msTillAlarm(imContext) && msTillAlarm(imContext) != 0) {
+            h.postDelayed(rStateTwo,100);
+        } else {
+            h.postDelayed(rStateTwo,IDLE_TIME_NEEDED /*1hr*/);
+        }
+        if (msTillAlarm(imContext) != 0) {
+            h.postDelayed(rStateThree,(msTillAlarm(imContext) - 900000));
+        }
+    }
+
+    public static void haltManager() {
+        String TAG_SUBCLASS = "haltManager";
+        h.removeCallbacks(rStateTwo);
+        theAwakening();
+    }
+
+    public static void theAwakening() {
+        String TAG_SUBCLASS = "theAwakening";
+        h.removeCallbacks(rStateThree);
+    }
+
+    public static long msTillAlarm(Context imContext) {
+        String TAG_SUBCLASS = "msTillAlarm";
+        AlarmManager.AlarmClockInfo info =
+                ((AlarmManager)imContext.getSystemService(Context.ALARM_SERVICE)).getNextAlarmClock();
+        if (info != null) {
+            long alarmTime = info.getTriggerTime();
+            long realTime = alarmTime - System.currentTimeMillis();
+            return realTime;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void servicesKiller() {
+        String TAG_SUBCLASS = "servicesKiller";
+        localActivityManager = (ActivityManager) imContext.getSystemService(Context.ACTIVITY_SERVICE);
+        RunningServices = localActivityManager.getRunningAppProcesses();
+        for (int i=0; i < RunningServices.size(); i++) {
+          if (!RunningServices.get(i).pkgList[0].toString().contains("com.android.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.google.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.mgoogle.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.vanced.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("zhihu.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("ugc.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("GoogleCamera.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.whatsapp.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.spotify.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("org.telegram") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.amazon.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.twitter.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.netflix.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.discord.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.reddit.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("org.thoughtcrime.securesms") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("chat.delta") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.wire") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.monzo.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.barclays.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.natwest.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("com.paypal.") &&
+                !RunningServices.get(i).pkgList[0].toString().contains("ims")) {
+                    localActivityManager.killBackgroundProcesses(RunningServices.get(i).pkgList[0].toString());
             }
         }
     }
+
 }
