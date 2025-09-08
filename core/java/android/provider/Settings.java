2ece924b62f6717cf64ad17fd6196e3f6e2a36c6
The-projects <simplncprivate@gmail.com>
2025-09-08 20:11:42 +0100

    Revert "SystemUI/Notification : IslandView"
    This reverts commit b884836cbaad4d12a2bccd4b8941484b75b4cf2d.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7136,11 +7136,6 @@ public final class Settings {
             return isCallingPackageAllowedToWriteSettings(context, Process.myUid(),
                     context.getOpPackageName(), false);
         }
-
-        /**
-         * @hide
-         */
-        public static final String ISLAND_NOTIFICATION = "island_notification";
     }
 
     /**
