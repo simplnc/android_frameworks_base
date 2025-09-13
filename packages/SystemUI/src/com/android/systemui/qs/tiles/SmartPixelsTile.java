cddabe22477e14fd4cd71ded3a7a287ff19a2b6f
theJecks <simplncprivate@gmail.com>
2025-09-13 08:09:08 +0100

    [1/2] SystemUI/QS : Smart Pixels and QSTile

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
@@ -159,7 +159,7 @@ public class SmartPixelsTile extends QSTileImpl<BooleanState> implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.EVOLVER;
+        return MetricsEvent.BASHA;
     }
 
     @Override
