81b83d491f042ef9999a53d7c8c7f7f0a17ecc3c
theJecks <simplncprivate@gmail.com>
2025-12-14 14:23:39 +0000

    [1/2] Display: add Ambient Customizations

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -93,7 +93,8 @@ public class AmbientDisplayConfiguration {
     /** @hide */
     public boolean pulseOnNotificationEnabled(int user) {
         return boolSettingDefaultOn(Settings.Secure.DOZE_ENABLED, user)
-                && pulseOnNotificationAvailable();
+                && pulseOnNotificationAvailable()
+                && !alwaysOnEnabled(user);
     }
 
     /** @hide */
